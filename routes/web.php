f9a98b2dbaf37202127fcfb383b4ef2f6b65bdd7
mihin <huguesaime.mihin@gmail.com>
2025-04-14 18:49:44 +0000

    config ok

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [AuthController::class, 'register'])->name('auth.register');
 
 Route::middleware('guest')->group(function () {
-    Route::get('/connexion', [AuthController::class, 'create'])->name('auth.create');
+    Route::get('/', [AuthController::class, 'create'])->name('auth.create');
     Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.login');
+    Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
     Route::post('/register/store', [AuthController::class, 'store'])->name('auth.store');
 });
 
